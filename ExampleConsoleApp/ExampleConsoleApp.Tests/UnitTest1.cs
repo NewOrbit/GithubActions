9e70530d53cba2caeddc378a1c586b8b8362bb25
Nick Murphy <nmurphy@neworbit.co.uk>
2022-02-24 09:59:32 +0000

    Adding test project with dummy tests

---
+++ ExampleConsoleApp/ExampleConsoleApp.Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExampleConsoleApp.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void PassingTest()
+        {
+            Assert.IsTrue(true);
+        }
+        [TestMethod]
+        public void FailingTest()
+        {
+            Assert.IsTrue(false);
+        }
+    }
+}
